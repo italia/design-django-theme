7adc2866c8e3ef79d7a1efef5dd0a6829ee5be56
Giuseppe De Marco <giuseppe.demarco@unical.it>
2020-03-06 10:54:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='design-django-theme',
-    version='v1.3.9-3',
+    version='v1.3.9-4',
     packages=['bootstrap_italia_template',],
     package_data = {'bootstrap_italia_template': ['bootstrap_italia_template/*']},
     include_package_data=True,
