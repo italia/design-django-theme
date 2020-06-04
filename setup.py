b1bf99f88e86b0992e8ff9551d9a7c93ac00ed75
Francesco Filicetti <francesco.filicetti@unical.it>
2020-06-04 11:31:30 +0200

    bootstrap-italia updated to v1.4.0

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='design-django-theme',
-    version='v1.3.10-1',
+    version='v1.4.0',
     packages=['bootstrap_italia_template',],
     package_data = {'bootstrap_italia_template': ['bootstrap_italia_template/*']},
     include_package_data=True,
