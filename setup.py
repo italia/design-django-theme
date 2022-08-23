9f247baf3cad0d16530a57bf0798a95ab50ddf10
Francesco Filicetti <francesco.filicetti@unical.it>
2022-08-23 11:17:58 +0200

    fix: bug in MANIFEST

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='design-django-theme',
-    version='v2.0.2',
+    version='v2.0.2-1',
     packages=['bootstrap_italia_template',],
     package_data = {'bootstrap_italia_template': ['bootstrap_italia_template/*']},
     include_package_data=True,
