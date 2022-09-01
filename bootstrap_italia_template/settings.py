e49614fab1bca272466dc6d92bbc0374062cc1ea
Francesco Filicetti <francesco.filicetti@unical.it>
2022-09-01 17:15:45 +0200

    fix: no cdn to load sprites.svg

--- bootstrap_italia_template/settings.py
+++ bootstrap_italia_template/settings.py
@@ -3,4 +3,4 @@ JS_DEFAULT_DATE_FORMAT = "dd/MM/yyyy"
 
 # CDN FOR STATICS
 DJANGO_BOOSTRAP_ITALIA_USE_CDN = False
-DJANGO_BOOSTRAP_ITALIA_CDN = 'https://cdn.jsdelivr.net/npm/bootstrap-italia@2.0.3/dist'
+DJANGO_BOOSTRAP_ITALIA_CDN = 'https://cdn.jsdelivr.net/npm/bootstrap-italia@2.0.3-1/dist'
