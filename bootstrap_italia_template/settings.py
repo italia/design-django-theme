e77284873644abc0c8cba49dbd1c1bf7e32a359a
Francesco Filicetti <francesco.filicetti@unical.it>
2023-05-09 08:49:29 +0200

    feat: Bootstrap italia v2.4.2

--- bootstrap_italia_template/settings.py
+++ bootstrap_italia_template/settings.py
@@ -3,4 +3,4 @@ JS_DEFAULT_DATE_FORMAT = "dd/MM/yyyy"
 
 # CDN FOR STATICS
 DJANGO_BOOSTRAP_ITALIA_USE_CDN = False
-DJANGO_BOOSTRAP_ITALIA_CDN = 'https://cdn.jsdelivr.net/npm/bootstrap-italia@2.3.6/dist'
+DJANGO_BOOSTRAP_ITALIA_CDN = 'https://cdn.jsdelivr.net/npm/bootstrap-italia@2.4.2/dist'
