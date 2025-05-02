9a6c7368cadeb28122e3b052601d856850e1aa1b
Francesco Filicetti <easyweb.wa@gmail.com>
2025-05-02 13:40:54 +0200

    fix: CDN URL in settings

--- bootstrap_italia_template/settings.py
+++ bootstrap_italia_template/settings.py
@@ -3,4 +3,4 @@ JS_DEFAULT_DATE_FORMAT = "dd/MM/yyyy"
 
 # CDN FOR STATICS
 DJANGO_BOOTSTRAP_ITALIA_USE_CDN = False
-DJANGO_BOOTSTRAP_ITALIA_CDN = 'https://cdn.jsdelivr.net/npm/bootstrap-italia@2.14.0-1/dist'
+DJANGO_BOOTSTRAP_ITALIA_CDN = 'https://cdn.jsdelivr.net/npm/bootstrap-italia@2.14.0/dist'
